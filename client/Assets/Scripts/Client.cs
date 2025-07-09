f6ee4f2e398ed135cc7308df8e5e996c87f39aab
Ilyas Khatipov <ilyaskhat@mail.ru>
2025-07-09 16:38:01 +0300

    added nicknames, foundation for animation and shoot handle in online mode

--- client/Assets/Scripts/Client.cs
+++ client/Assets/Scripts/Client.cs
@@ -47,7 +47,6 @@ public class Client : MonoBehaviour
         MessagePackSerializer.DefaultOptions = options;
         
         InitializeNetwork();
-        ConnectToServer();
     }
 
     void InitializeNetwork()
